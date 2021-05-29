2cf527d3842e9382dc4a2128c5f264d937c0aaab
furkanhalidi <33151546+furkanhalidi@users.noreply.github.com>
2021-05-29 13:38:09 +0300

    first

---
+++ LandmarkBook/app/src/main/java/com/furkanhalidi/landmarkbook/Singleton.java
@@ -0,0 +1,30 @@
+package com.furkanhalidi.landmarkbook;
+
+import android.graphics.Bitmap;
+
+public class Singleton {
+
+    private Bitmap chosenImage;
+    private static Singleton singleton;
+
+    public Singleton() {
+
+    }
+
+    public Bitmap getChosenImage() {
+        return chosenImage;
+    }
+
+    public void setChosenImage(Bitmap chosenImage) {
+        this.chosenImage = chosenImage;
+    }
+
+    public static Singleton getInstance(){
+
+        if (singleton == null){
+            singleton = new Singleton();
+        }
+
+        return singleton;
+    }
+}
